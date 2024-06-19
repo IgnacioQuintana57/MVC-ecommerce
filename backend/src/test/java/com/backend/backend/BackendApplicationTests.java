1efb959068e80500de45b14b85e9d7ad9607a73f
Ignacio Quintana <ignacioquintanajcc@gmail.com>
2024-06-19 17:29:42 -0300

    test2

--- backend/src/test/java/com/backend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/backend/backend/BackendApplicationTests.java
@@ -1,22 +1,10 @@
 package com.backend.backend;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootTest
 class BackendApplicationTests {
-
-	@BeforeAll
-	static void setUp() {
-		Dotenv dotenv = Dotenv.configure().directory("src/main/resources").load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-
-	}
 
 	@Test
 	void contextLoads() {
